c2981bb1334973d81ae493f3ac4a4c27d7269fee
Vinícius Simões <vinicius.simoes95@gmail.com>
2018-08-27 22:53:42 -0300

    Class 39 - Add Login page and token handlers

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-/* global XMLHttpRequest */
+/* global XMLHttpRequest localStorage */
 var app = {}
 
 app.config = {
@@ -67,51 +67,165 @@ app.client.request = function (headers, path, method, queryStringObject, payload
 }
 
 app.bindForms = function () {
-  document.querySelector('form').addEventListener('submit', function (e) {
-    e.preventDefault()
+  if (document.querySelector('form')) {
+    document.querySelector('form').addEventListener('submit', function (e) {
+      e.preventDefault()
 
-    var formId = this.id
-    var path = this.action
-    var method = this.method.toUpperCase()
+      var formId = this.id
+      var path = this.action
+      var method = this.method.toUpperCase()
 
-    document.querySelector('#' + formId + ' .formError').style.display = 'hidden'
+      document.querySelector('#' + formId + ' .formError').style.display = 'hidden'
 
-    var payload = {}
-    var elements = this.elements
+      var payload = {}
+      var elements = this.elements
 
-    for (var i = 0; i < elements.length; i++) {
-      if (elements[i].type !== 'submit') {
-        var valueOfElement = elements[i].type === 'checkbox' ? elements[i].checked : elements[i].value
+      for (var i = 0; i < elements.length; i++) {
+        if (elements[i].type !== 'submit') {
+          var valueOfElement = elements[i].type === 'checkbox' ? elements[i].checked : elements[i].value
 
-        payload[elements[i].name] = valueOfElement
+          payload[elements[i].name] = valueOfElement
+        }
       }
-    }
 
-    app.client.request(undefined, path, method, undefined, payload, function (statusCode, responsePayload) {
-      if (statusCode !== 200) {
-        var error = typeof (responsePayload.Error) === 'string' ? responsePayload.Error : 'An error has ocorred, please try again.'
+      app.client.request(undefined, path, method, undefined, payload, function (statusCode, responsePayload) {
+        if (statusCode !== 200) {
+          var error = typeof (responsePayload.Error) === 'string' ? responsePayload.Error : 'An error has ocorred, please try again.'
 
-        document.querySelector('#' + formId + ' .formError').innerHTML = error
+          document.querySelector('#' + formId + ' .formError').innerHTML = error
 
-        document.querySelector('#' + formId + ' .formError').style.display = 'block'
-      } else {
-        app.formResponseProcessor(formId, payload, responsePayload)
-      }
+          document.querySelector('#' + formId + ' .formError').style.display = 'block'
+        } else {
+          app.formResponseProcessor(formId, payload, responsePayload)
+        }
+      })
     })
-  })
+  }
 }
 
 app.formResponseProcessor = function (formId, requestPayload, responsePayload) {
   var functionToCall = false
 
   if (formId === 'accountCreate') {
-    // @TODO Do something here now that the account has been created successfully
-    console.log('Account has been created')
+    var newPayload = {
+      phone: requestPayload.phone,
+      password: requestPayload.password
+    }
+
+    app.client.request(undefined, 'api/tokens', 'POST', undefined, newPayload, function (newStatusCode, newResponsePayload) {
+      if (newStatusCode !== 200) {
+        document.querySelector('#' + formId + ' .formError').innerHTML = 'Sorry, an error has ocurred. Please try again.'
+
+        document.querySelector('#' + formId + ' .formError').style.display = 'block'
+      } else {
+        app.setSessionToken(newResponsePayload)
+        window.location = '/checks/all'
+      }
+    })
   }
+
+  if (formId === 'sessionCreate') {
+    app.setSessionToken(responsePayload)
+    window.location = '/checks/all'
+  }
+}
+
+app.getSessionToken = function () {
+  var tokenString = localStorage.getItem('token')
+
+  if (typeof (tokenString) === 'string') {
+    try {
+      var token = JSON.parse(tokenString)
+
+      app.config.sessionToken = token
+
+      if (typeof (token) === 'object') {
+        app.setLoggedInClass(true)
+      } else {
+        app.setLoggedInClass(false)
+      }
+    } catch (err) {
+      app.config.sessionToken = false
+      app.setLoggedInClass(false)
+    }
+  }
+}
+
+app.setLoggedInClass = function (add) {
+  var target = document.querySelector('body')
+
+  if (add) {
+    target.classList.add('loggedIn')
+  } else {
+    target.classList.remove('loggedIn')
+  }
+}
+
+app.setSessionToken = function (token) {
+  app.config.sessionToken = token
+
+  var tokenString = JSON.stringify(token)
+
+  localStorage.setItem('token', tokenString)
+
+  if (typeof (token) === 'object') {
+    app.setLoggedInClass(true)
+  } else {
+    app.setLoggedInClass(false)
+  }
+}
+
+app.renewToken = function (callback) {
+  var currentToken = typeof (app.config.sessionToken) === 'object' ? app.config.sessionToken : false
+
+  if (currentToken) {
+    var payload = {
+      id: currentToken.id,
+      extend: true
+    }
+
+    app.client.request(undefined, 'api/tokens', 'PUT', undefined, payload, function (statusCode, responsePayload) {
+      if (statusCode === 200) {
+        var queryStringObject = {
+          id: currentToken.id
+        }
+
+        app.client.request(undefined, 'api/tokens', 'GET', queryStringObject, undefined, function (newStatusCode, newResponsePayload) {
+          if (newStatusCode === 200) {
+            app.setSessionToken(newResponsePayload)
+
+            callback(false)
+          } else {
+            app.setSessionToken(false)
+
+            callback(true)
+          }
+        })
+      } else {
+        app.setSessionToken(false)
+
+        callback(true)
+      }
+    })
+  }
+}
+
+app.tokenRenewalLoop = function () {
+  setInterval(function () {
+    app.renewToken(function (err) {
+      if (err) {
+        console.log('Token renewed successfully @ ' + Date.now())
+      }
+    })
+  }, 1000 * 60)
 }
 
 app.init = function () {
   app.bindForms()
+
+  app.getSessionToken()
+
+  app.tokenRenewalLoop()
 }
 
 window.onload = function () {
