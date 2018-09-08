ef135cade757628e1b12f581847f5fb884b41c73
Vinícius Simões <vinicius.simoes95@gmail.com>
2018-09-08 10:24:54 -0300

    Class 48 - adding cli

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 var server = require('./lib/server')
 var workers = require('./lib/workers')
+var cli = require('./lib/cli')
 
 var app = {}
 
 app.init = function () {
   server.init()
   workers.init()
+
+  setTimeout(function () {
+    cli.init()
+  }, 50)
 }
 
 app.init()
